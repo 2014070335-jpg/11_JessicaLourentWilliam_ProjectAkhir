569b84306c765fd8db0bdccb58d3adb59f67d48f
2014070335-jpg <2014070335@student.pppkpetra.sch.id>
2025-11-17 09:08:11 +0700

    Add files via upload

---
+++ beasiswa.sql
@@ -0,0 +1,105 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.2
+-- https://www.phpmyadmin.net/
+--
+-- Host: localhost
+-- Generation Time: Nov 17, 2025 at 02:05 AM
+-- Server version: 8.0.43
+-- PHP Version: 8.2.29
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `beasiswa`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `admin`
+--
+
+CREATE TABLE `admin` (
+  `id` int NOT NULL,
+  `nama` varchar(200) COLLATE utf8mb4_general_ci NOT NULL,
+  `email` varchar(200) COLLATE utf8mb4_general_ci NOT NULL,
+  `notelp` int NOT NULL,
+  `password` varchar(200) COLLATE utf8mb4_general_ci NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `admin`
+--
+
+INSERT INTO `admin` (`id`, `nama`, `email`, `notelp`, `password`) VALUES
+(1, 'jes', 'jessicalourent2007@gmail.com', 8748278, '1212jl');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `member`
+--
+
+CREATE TABLE `member` (
+  `id` int NOT NULL,
+  `peserta` varchar(300) COLLATE utf8mb4_general_ci NOT NULL,
+  `tgl_lahir` date NOT NULL,
+  `nilai_rata` float NOT NULL,
+  `status_berkas` tinyint(1) NOT NULL,
+  `file_berkas` varchar(500) COLLATE utf8mb4_general_ci NOT NULL,
+  `waktu_daftar` timestamp NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `member`
+--
+
+INSERT INTO `member` (`id`, `peserta`, `tgl_lahir`, `nilai_rata`, `status_berkas`, `file_berkas`, `waktu_daftar`) VALUES
+(1, 'jessica', '2025-11-14', 0.11, 1, '1763173859_8700cc25f795.png', '2025-11-15 02:30:59'),
+(2, 'jessica', '2025-11-14', 0.11, 1, '1763176444_5661d5c521af.png', '2025-11-15 03:14:04'),
+(3, 'jessica', '2025-11-14', 0.11, 0, '1763344105_d289e080dd7b.png', '2025-11-17 01:48:25');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `admin`
+--
+ALTER TABLE `admin`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `member`
+--
+ALTER TABLE `member`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `admin`
+--
+ALTER TABLE `admin`
+  MODIFY `id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- AUTO_INCREMENT for table `member`
+--
+ALTER TABLE `member`
+  MODIFY `id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
